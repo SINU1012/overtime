dd0691287b4100d6eb96f7f5e6a67b0a8c820288
shinwoo <tlsdn1012@gmail.com>
2025-02-27 17:56:01 +0900

    Initial commit

--- config/firebase.js
+++ config/firebase.js
@@ -5,11 +5,11 @@ const { getFirestore } = require("firebase-admin/firestore");
 let serviceAccount;
 
 if (process.env.SERVICE_ACCOUNT_KEY) {
-  // 배포 환경: JSON을 환경변수로 넣어두고, 여기서 parse
+  // 배포 환경: JSON 문자열을 환경변수로 넣는다
   serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
 } else {
-  // 로컬 개발용 (파일로부터)
-  serviceAccount = require("./serviceAccountKey.json");
+  // 로컬 개발 환경: 파일 사용
+  serviceAccount = require(".serviceAccountKey.json");
 }
 
 const app = initializeApp({
